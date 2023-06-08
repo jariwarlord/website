bca9bb84ef4869235b6e7b5e6715bdb27a76f977
Berke Dalar <berkedalar4@gmail.com>
2023-06-08 23:23:12 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+<form action = "functions.php" method="get">
+    <input type="text" name = "num01" placeholder="Number 1">
+   <select name="oper">
+   <label>Choose Operation</label> 
+    <option value ="add">Add</option>
+    <option value="Substract">Subsctract</option>
+</select>
+<input type="text" name="num02" placeholder = "Number 2">
+<button type="submit">Calculate</button>
+</form>
+        
+        <script src="" async defer></script>
+    </body>
+</html>
